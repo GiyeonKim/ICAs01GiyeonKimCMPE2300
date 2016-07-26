49a331b870111e7da34450372baad754889befef
Pinkmong <gkim9@studentmail.nait.ca>
2016-07-25 19:06:12 -0600

    CMPE 2300 ICA 1
    TrekLights program

---
+++ ICAs01GiyeonKimCMPE2300/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace ICAs01GiyeonKimCMPE2300
+{
+    public partial class Form1 : Form
+    {
+        CDrawer Drawing = new CDrawer(900, 500);
+        Random RandomNum = new Random();
+        
+
+        List<TrekLamp> Mylist = new List<TrekLamp>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            Drawing.ContinuousUpdate = false;
+            Drawing.Scale = 50; 
+            timer1.Enabled = true; // don't stop the timer
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1 && Mylist.Count < 180)
+            {
+                
+                Mylist.Add(new TrekLamp()); // set it as default
+            }
+
+            if(e.KeyCode == Keys.F2 && Mylist.Count < 180)
+            {
+                Mylist.Add(new TrekLamp(180,Color.Orange,4)); 
+            }
+            if(e.KeyCode == Keys.F3 && Mylist.Count < 180)
+            {
+                Mylist.Add(new TrekLamp((byte)RandomNum.Next(60,220), RandColor.GetColor(), 4));
+            }
+            if(e.KeyCode == Keys.Escape && Mylist.Count > 0)
+            {
+                Mylist.RemoveAt(Mylist.Count -1);  
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Drawing.Clear(); // clear the CDrawer first
+            for (int i =0; i < Mylist.Count; i++ ) // list
+            {
+                Mylist[i].Tick();
+                Mylist[i].RenderLamp(Drawing, i);
+               
+            }
+            Drawing.Render();
+
+        }
+    }
+}
---
+++ ICAs01GiyeonKimCMPE2300/TrekLamp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace ICAs01GiyeonKimCMPE2300
+{
+    class TrekLamp
+    {
+        private Color _LampColor; //Type of color
+        private byte _toggle;//type of toggle turn on or off
+        private byte _byTick =0; // type of ticks which iterates over the allowed byte range
+        private int _border; //type of int
+
+
+
+        public TrekLamp(byte toggle, Color ColorofLamp, int Border =2) 
+        {
+            //default parametter of 2 for border argument
+            //In the body, initialize their respective corresponding member values
+            //byte[] rand = new byte[1];
+            _border = Border; 
+            _toggle = toggle;
+            
+            Random random = new Random(); //making new random generator
+             
+            _byTick = (byte)random.Next(byte.MaxValue+1);//assign _bytick to start at a random
+            _LampColor = ColorofLamp;//random generage color
+
+       
+        }
+
+        public TrekLamp() : this(64, RandColor.GetColor(),6)
+        {
+            // private TrekLamp() : this(64) //child of drives from whatever what's after the colon
+            //  {
+            //add a default constructor, using leverage your custom constructor with a 
+            //threshold value of 64
+            //  }
+        }
+       
+        public void Tick()
+        {
+
+            _byTick += 3;
+            // increment tick by 3
+
+        }
+
+        public void RenderLamp(CDrawer Drawing, int values)
+        {
+            
+
+            if (values <= 180 && _byTick > _toggle)
+            {
+                int Col = values % 18; // it's 18 x 10 not 16 x 12
+                int row = values / 18;
+                Drawing.AddRectangle(Col, row, 1 /*Drawing.Scale*/, 1/*Drawing.Scale*/, _LampColor, _border, Color.Black);
+            }
+                    
+            
+            
+        }
+
+    }
+}
+
+
+
